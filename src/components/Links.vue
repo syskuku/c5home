7bf0420d868de77f757fd82ab132aeb928e1f63b
baishu <80586922+baishu12@users.noreply.github.com>
2024-07-08 04:18:45 +0000

    增加账户管理

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link,BloggerB, UserFriends, Rocketchat, Cloud, Robot, Check, Heart } from "@vicons/fa"; // 注意使用正确的类别
+import { Link,BloggerB, UserFriends, Rocketchat, Cloud, Robot, Check, Heart,User} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -69,7 +69,8 @@ const siteIcon = {
   Rocketchat,
   Robot,
   Check,
-  Heart
+  Heart,
+  User
 };
 
 // 链接跳转
