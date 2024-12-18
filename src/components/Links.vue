fa573605535568de63f54ee1a67d087a8cd769e8
jwb <jiangwenbin@readboy.com>
2024-12-18 14:08:25 +0800

    网站链接 添加Bitwarden

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Desktop } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Desktop, Key } from "@vicons/fa"; // 注意使用正确的类别
 import { Message } from "@vicons/tabler"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
@@ -74,6 +74,7 @@ const siteIcon = {
   Comment,
   Microblog,
   Desktop,
+  Key,
 };
 
 // 链接跳转
